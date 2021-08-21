46d6c1ce97349bf82aa232bfdfd98988be240286
Michał Małek <michal.malek98@wp.pl>
2021-08-21 15:38:52 +0200

    completed sign in form

--- src/app/Components/sign-in/sign-in.component.ts
+++ src/app/Components/sign-in/sign-in.component.ts
@@ -3,29 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
 
   isActive = true;
   hide = true;
-
   loading = false;
+  isDisabled = false;
+  form = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  private delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   public loadRegisterForm() {
-    let panel = document.getElementById('sign-in-panel') as HTMLElement;
-
-    panel.style.backgroundColor = 'background-color: rgba(255, 255, 255, 0.1);';
-    panel.style.filter = 'blur(4px)';
-    
+    let element = document.getElementById('clickable-elements') as HTMLElement;
+    element.style.marginBottom = '5%';
     this.loading = true;
+    
+    setTimeout(() => {
+      this.loading = false;
+      element.style.marginBottom = '17%';
+    }, 1500);
 
-    console.log("hello");
+    
+  }
+
+  public loadForgotPassword() {
+    
+  }
+
+  public loadSupportDialog() {
+
+  }
+
+  public loadContactDialog() {
+
+  }
+
+  public loadTermsOfUseDialog() {
+
   }
 
 }
