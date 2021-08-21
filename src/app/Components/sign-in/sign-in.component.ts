a0030c49608b4b85d6166a965dd788ca60f166ae
Michał Małek <michal.malek98@wp.pl>
2021-08-21 16:44:40 +0200

    final register form

--- src/app/Components/sign-in/sign-in.component.ts
+++ src/app/Components/sign-in/sign-in.component.ts
@@ -9,17 +9,13 @@ export class SignInComponent implements OnInit {
 
   isActive = true;
   hide = true;
+  hideSecond = true;
   loading = false;
-  isDisabled = false;
-  form = false;
+  registerForm = false;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  private delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   public loadRegisterForm() {
@@ -30,9 +26,40 @@ export class SignInComponent implements OnInit {
     setTimeout(() => {
       this.loading = false;
       element.style.marginBottom = '17%';
-    }, 1500);
+      this.loadRegisterGui();
+    }, 1100);
+  }
 
+  public loadRegisterGui() {
+    let windowName = document.getElementById('label') as HTMLElement;
+    windowName.innerHTML = 'Sign Up'
+
+    let icon = document.getElementById('person-icon') as HTMLElement;
+    icon.innerHTML = 'person_add';
+
+    this.registerForm = true;
+  }
+
+  public loadLoginGui() {
+    let windowName = document.getElementById('label') as HTMLElement;
+    windowName.innerHTML = 'Sign In'
+
+    let icon = document.getElementById('person-icon') as HTMLElement;
+    icon.innerHTML = 'person';
+
+    this.registerForm = false;
+  }
+  
+  public loadLoginForm() {
+    let element = document.getElementById('clickable-elements') as HTMLElement;
+    element.style.marginBottom = '5%';
+    this.loading = true;
     
+    setTimeout(() => {
+      this.loading = false;
+      element.style.marginBottom = '17%';
+      this.loadLoginGui();
+    }, 1100);
   }
 
   public loadForgotPassword() {
