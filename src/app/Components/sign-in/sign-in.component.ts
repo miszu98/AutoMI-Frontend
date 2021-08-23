4fc456b991364f01a58df0ebc7526cc818811883
Michał Małek <michal.malek98@wp.pl>
2021-08-23 16:20:01 +0200

    start working on main page

--- src/app/Components/sign-in/sign-in.component.ts
+++ src/app/Components/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { InformationsComponent } from '../Dialogs/informations/informations.component';
 
 @Component({
   selector: 'app-sign-in',
@@ -12,7 +13,8 @@ export class SignInComponent implements OnInit {
   hide = true;
   hideSecond = true;
   loading = false;
-  registerForm = false;
+  flag = false;
+  forgotPassword = false;
 
   constructor(private dialog: MatDialog) { }
 
@@ -20,6 +22,7 @@ export class SignInComponent implements OnInit {
   }
 
   public loadRegisterForm() {
+    this.forgotPassword = false;
     let element = document.getElementById('clickable-elements') as HTMLElement;
     element.style.marginBottom = '5%';
     this.loading = true;
@@ -32,26 +35,29 @@ export class SignInComponent implements OnInit {
   }
 
   public loadRegisterGui() {
+    this.forgotPassword = false;
     let windowName = document.getElementById('label') as HTMLElement;
     windowName.innerHTML = 'Sign Up'
 
     let icon = document.getElementById('person-icon') as HTMLElement;
     icon.innerHTML = 'person_add';
 
-    this.registerForm = true;
+    this.flag = true;
   }
 
   public loadLoginGui() {
+    this.forgotPassword = false;
     let windowName = document.getElementById('label') as HTMLElement;
     windowName.innerHTML = 'Sign In'
 
     let icon = document.getElementById('person-icon') as HTMLElement;
     icon.innerHTML = 'person';
 
-    this.registerForm = false;
+    this.flag = false;
   }
   
   public loadLoginForm() {
+    this.forgotPassword = false;
     let element = document.getElementById('clickable-elements') as HTMLElement;
     element.style.marginBottom = '5%';
     this.loading = true;
@@ -64,19 +70,26 @@ export class SignInComponent implements OnInit {
   }
 
   public loadForgotPassword() {
+    let windowName = document.getElementById('label') as HTMLElement;
+    windowName.innerHTML = 'Change Password';
+
+    let icon = document.getElementById('person-icon') as HTMLElement;
+    icon.innerHTML = 'lock';
     
+
+    this.forgotPassword = true;
   }
 
   public loadSupportDialog() {
-    
+    const dialogRef = this.dialog.open(InformationsComponent);
   }
 
   public loadContactDialog() {
-
+    const dialogRef = this.dialog.open(InformationsComponent);
   }
 
   public loadTermsOfUseDialog() {
-
+    const dialogRef = this.dialog.open(InformationsComponent);
   }
 
 
