6520b45e9b3df576c58bf8060ab6af47725d20cc
Michał Małek <michal.malek98@wp.pl>
2021-08-28 11:53:48 +0200

    created mark service and method which get all marks in database

--- src/app/Components/main/main.component.ts
+++ src/app/Components/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Mark } from 'src/app/Models/Mark';
+import { MarksService } from 'src/app/Services/Marks/marks.service';
 
 @Component({
   selector: 'app-main',
@@ -9,10 +11,23 @@ export class MainComponent implements OnInit {
 
   title = 'AutoMI';
   showMenu = false;
+  declare marks: Array<Mark>;
 
-  constructor() { }
+  constructor(private markService: MarksService) { }
 
   ngOnInit(): void {
+    this.getAllMarks();
+  }
+
+  public getAllMarks() {
+    this.markService.getAll().subscribe(
+      value => {
+        this.marks = value;
+      }, 
+      error => {
+        console.log("Error while trying get all marks from database")
+      }
+    );
   }
 
 
