4c5b292f95d0d8f09dd06ca21ecd3c6534724b6a
Michał Małek <michal.malek98@wp.pl>
2021-08-30 18:50:08 +0200

    modified path to Gearbox model

--- src/app/Services/Gearboxes/gearboxes.service.ts
+++ src/app/Services/Gearboxes/gearboxes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Gearbox } from 'src/app/Models/CarOffer';
+import { Gearbox } from 'src/app/Models/Gearbox';
 
 @Injectable({
   providedIn: 'root'
