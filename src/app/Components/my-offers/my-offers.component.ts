b0266b331c47e9a607db589169588829bcfe5c0e
Michał Małek <michal.malek98@wp.pl>
2021-09-05 17:08:20 +0200

    started work on my offers page

--- src/app/Components/my-offers/my-offers.component.ts
+++ src/app/Components/my-offers/my-offers.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-offers.component.css']
 })
 export class MyOffersComponent implements OnInit {
+  
+  title = "AutoMI";
 
   constructor() { }
 
