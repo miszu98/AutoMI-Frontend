68c7518151c264b83117434144feb122ea049d4d
Michał Małek <michal.malek98@wp.pl>
2021-09-25 17:59:40 +0200

    added path to Offer details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { SignInComponent } from './Components/sign-in/sign-in.component';
 import { Router, RouterModule } from '@angular/router';
 import { MainComponent } from './Components/main/main.component';
 import { MyOffersComponent } from './Components/my-offers/my-offers.component';
+import { OfferDetailsComponent } from './Components/OfferDetails/offer-details/offer-details.component';
 
 const routes = [
   {path: 'sign-in', component: SignInComponent},
   {path: 'main', component: MainComponent},
-  {path: 'my-offers', component: MyOffersComponent}
+  {path: 'my-offers', component: MyOffersComponent},
+  {path: ':id/offer?search', component: OfferDetailsComponent}
 ];
 
 @NgModule({
